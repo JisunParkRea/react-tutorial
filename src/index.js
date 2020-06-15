1c422ce5ced80b93eb873c1153471a629f3a3b7c
Jisun Park <fpdldk912@gmail.com>
2020-06-15 17:17:03 +0900

    make interactive component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
 class Square extends React.Component {
+  // sub class 생성자 정의
+  constructor(props) {
+    super(props); // 항상 super 메서드 명시
+    this.state = {
+      value: null,
+    };
+  }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
-      );
+      ); // square 클릭시 X값으로 바뀌게하기
     }
   }
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />; // value값을 전달받을 수 있게
     }
   
     render() {
